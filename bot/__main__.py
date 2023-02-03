80a2c36a86b905d662d45e59cdb234a33aac7ccd
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2023-02-04 03:01:34 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -172,21 +172,15 @@ def stats(update, context):
                     f'<b>╰ 🔻 Download Data:</b> {recv}\n\n'
 
     else:
-            stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-                    f'<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
-                    f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
-                    f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-                    f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-                    f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
-                    f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
-                    f'<b>RAM:</b> {memory.percent}%\n'\
-                    f'<b>DISK:</b> {disk}%\n\n'\
-                    f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
-                    f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
-                    f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
-                    f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
-                    f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
-                    f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n'
+            stats = f'<b>Commit Date: </b>{last_commit}\n\n'\
+                    f'<b>Bot Uptime: </b>{currentTime}\n'\
+                    f'<b>OS Uptime: </b>{osUptime}\n\n'\
+                    f'<b>CPU: </b>{cpuUsage}%\n'\
+                    f'<b>RAM: </b>{mem_p}%\n'\
+                    f'<b>Disk: </b>{disk}%\n'\
+                    f'<b>Disk Free: </b>{free}\n\n'\
+                    f'<b>Upload Data: </b>{sent}\n'\
+                    f'<b>Download Data: </b>{recv}\n'
 
 
 
